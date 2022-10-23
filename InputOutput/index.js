5065cbf547f8ab00b094af484ed2f60fd8861219
Tanuj Patel <88612102+Tanujp106@users.noreply.github.com>
2022-10-23 22:22:49 +0530

    Update index.js

--- InputOutput/index.js
+++ InputOutput/index.js
@@ -4,11 +4,23 @@ const returnfun = () => {
   const textnode = document.createTextNode(x);
   node.appendChild(textnode);
   document.getElementById("myList").appendChild(node);
+  node.style.backgroundColor = "blue";
   document.getElementById("input").value = " ";
 
-  const del = document.createElement("button");
-  del.setAttribute("hey","buttoan");
+  var del = document.createElement("button");
+  del.setAttribute("id", "hello");
+  del.textContent = "Delete";
   document.getElementById("myList").appendChild(del);
+
+  const edit = document.createElement("button");
+  edit.setAttribute("id","editt");
+  edit.textContent = "Edit";
+  document.getElementById("myList").appendChild(edit);
+
+  const save = document.createElement("button");
+  save.setAttribute("id","button");
+  save.textContent = "Save";
+  document.getElementById("myList").appendChild(save);
 
   del.addEventListener("click", myFunction);
   function myFunction() {
@@ -16,5 +28,25 @@ const returnfun = () => {
     node.remove();
     document.getElementById("del");
     del.remove();
+    document.getElementById("editt");
+    edit.remove();
+    document.getElementById("save");
+    save.remove();
   }
+  save.addEventListener("click", savefunction);
+  function savefunction(){
+    document.getElementById("save");
+    save.remove();
+  }
+
+  edit.addEventListener("click", function() {
+  node.contentEditable = true;
+  node.style.backgroundColor = "yellow";
+  document.getElementById("myList").appendChild(save);
+} );
+
+  save.addEventListener("click", function() {
+  node.contentEditable = false;
+  node.style.backgroundColor = "gray";
+} )
 }
