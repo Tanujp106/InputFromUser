2a99ef2fae127fe5c30daf7db0106eaff95e853b
Tanuj Patel <88612102+Tanujp106@users.noreply.github.com>
2022-11-26 11:27:12 +0530

    Delete index2.js

--- index2.js
+++
@@ -1,9 +0,0 @@
-   let y = localStorage.getItem('tasklist')
-   document.getElementById("output").textContent += y;
-
-   document.getElementById("butn").addEventListener("click", function(){
-   localStorage.setItem('task','t');
-   })
-
-
-
