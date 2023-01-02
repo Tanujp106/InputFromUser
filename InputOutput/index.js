947f051f72c3f7af3f3677a0109023a6aebfae1d
Tanuj Patel <88612102+Tanujp106@users.noreply.github.com>
2023-01-02 09:25:08 +0530

    Update index.js

--- InputOutput/index.js
+++ InputOutput/index.js
@@ -1,15 +1,20 @@
-const returnfun = () => {
-  const x = document.getElementById("input").value;
-  const node = document.createElement("li");
-  const textnode = document.createTextNode(x);
-  node.appendChild(textnode);
+let node = document.createElement("li");
+    const returnfun = () => {
+    const x = document.getElementById("input").value; 
+    const textnode = document.createTextNode(x);
+    node.appendChild(textnode);
+    // returnfun();
+};
+
+const showfun = () => {
+    // document.getElementById(node).style.display = "none";
+    
   document.getElementById("myList").appendChild(node);
   document.getElementById("input").value = " ";
 
-  const del = document.createElement("button");
-  del.setAttribute("hey","buttoan");
+   const del = document.createElement("button");
+  del.setAttribute("hey", "buttoan");
   document.getElementById("myList").appendChild(del);
-
   del.addEventListener("click", myFunction);
   function myFunction() {
     document.getElementById("node");
